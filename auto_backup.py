b09f8b09a1111f9e362d4822be86f2c88237d65f
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2019-10-31 22:52:39 +0800

    updated auto_backup script
    * added arguments holder
    * added directory list holder

--- auto_backup.py
+++ auto_backup.py
@@ -1,6 +1,7 @@
 import pysftp
 import os
 import settings
+import sys
 from stat import S_ISDIR, S_ISREG
 
 class AutoBackup:
@@ -53,31 +54,41 @@ class AutoBackup:
     # Option Setter
     @option.setter
     def option(self, x):
-        self._option = x
+        self._option = x  
 
     # Connection to SFTP
     def start(self, remote_dir, local_dir, preserve_mtime=False):
+        print('Connection successfully to HBL remote server established ...')
+
         with pysftp.Connection(
             self.host, 
             username=self.username, 
             password=self.password, 
             cnopts=self.option
         ) as sftp:
-            print('Connection successfully established ...')
+            capture_index = None
+            directories = []
 
-            for entry in sftp.listdir_attr(remote_dir):
+            for index, entry in enumerate(sftp.listdir_attr(remote_dir)):
+                capture_index = index
+
+                print('Index: {} Downloading {}'.format(index, entry.filename))
+
                 remote_path = remote_dir + '/' + entry.filename
                 local_path = os.path.join(local_dir, entry.filename) 
                 mode = entry.st_mode
 
+                print('Mode: {}'.format(mode))
+                print('Directory? {}'.format(S_ISDIR(mode)))
+
                 if S_ISDIR(mode):
-                    try:
-                        os.mkdir(local_path)
-                    except OSError:
-                        pass
-                    self.start(remote_dir, local_path, preserve_mtime)      
+                    directories.append(entry.filename)      
                 elif S_ISREG(mode):
                     sftp.get(remote_path, local_path, preserve_mtime=preserve_mtime)
+
+            for directory in enumarate(directories):
+                print(directory)
+             
     
 if __name__ == '__main__':
     cnopts = pysftp.CnOpts()
@@ -89,8 +100,8 @@ if __name__ == '__main__':
         cnopts   
     )
     auto_backup.start(
-        '/home/hbladmin/hbposts.com/', 
-        'D:\Programming\hbl\local_backup',
+        sys.argv[1], 
+        sys.argv[2],
         preserve_mtime=False
     )
 
